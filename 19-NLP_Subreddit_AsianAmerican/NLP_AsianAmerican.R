d0a4f2a7526bd6844cbda10f3145bc2361be6e50
Oscar Ko <OscarKoEmail@gmail.com>
2022-10-01 11:48:37 -0400

    Day 93: NLP text cleaning. LDA topic analysis. Visualize topic clusters.

--- 19-NLP_Subreddit_AsianAmerican/NLP_AsianAmerican.R
+++ 19-NLP_Subreddit_AsianAmerican/NLP_AsianAmerican.R
@@ -101,293 +101,231 @@ any(is.na(df))
 
 
 
-############################################
-############
-######
+##############################################################
+##############################################################
+# NLP SECTIONS
 
-# 
-# library(tidyverse)
-# 
-# 
-# 
-# ###################################################
-# # DATA CLEANING
-# 
-# # Separate titles into tags and prompts ------------
-# 
-# tags <- c()
-# prompts <- c()
-# 
-# for (txt in title_df$title) {
-#   
-#   split_txt = strsplit(txt, "]")
-# 
-#   tag = substring(split_txt[[1]][1], 2)
-#   prompt = split_txt[[1]][2]
-# 
-#   tags <- c(tags, tag)
-#   prompts <- c(prompts, prompt)
-# }
-# 
-# 
-# title_df$tag <- tags
-# 
-# title_df$prompt <- prompts
-# 
-# head(title_df)
-# 
-# unique(title_df$tag)
-# 
-# 
-# # Fix a row anomoly -----------------------------------------------
-# 
-# error_tag <- " man who sees ghosts checks himself into a mental institution, oblivious to the fact that the facility has been closed for almost thirty years. [WP"
-# 
-# title_df[title_df$tag == error_tag, ]$title
-# 
-# the_title <- "[WP] A man who sees ghosts checks himself into a mental institution, oblivious to the fact that the facility has been closed for almost thirty years."
-# 
-# the_tag <- "WP"
-# 
-# the_prompt <- "A man who sees ghosts checks himself into a mental institution, oblivious to the fact that the facility has been closed for almost thirty years." 
-# 
-# title_df <- title_df[title_df$tag != error_tag, ]
-# 
-# title_df <- rbind(title_df, c(the_title, the_tag, the_prompt))
-# 
-# tail(title_df)
-# 
-# 
-# # Keep only standard [WP] posts. Filter out everything else. --------
-# 
-# unique(title_df$tag)
-# 
-# title_df <- title_df[title_df$tag %in% c("WP", "Wp", " WP "), ]
-# 
-# unique(title_df$tag)
-# 
-# nrow(title_df)
-# 
-# 
-# # Create prompts dataframe ------------------------------------------
-# 
-# prompt_df <- select(title_df, id, prompt, score, num_comments, url, created)
-# 
-# head(prompt_df)
-# tail(prompt_df)
-# 
-# # write.csv(prompt_df,
-# #           file = paste("cleaned_prompt_df"
-# #                        ".csv",
-# #                        sep = ""))
-# 
-# 
-# ##############################################################
-# ##############################################################
-# # NLP SECTIONS
-# 
-# 
-# ##############################################################
-# # CREATE CORPUS 
-# 
-# 
-# # create document id -------------------
-# 
-# # note: for some reason this is quotes sensitive, 
-# # use double quotes
-# names(prompt_df)[names(prompt_df)=="id"] <- "doc_id"
-# prompt_df$id <- as.character(prompt_df$id)
-# 
-# names(prompt_df)[names(prompt_df)=="prompt"] <- "text"
-# colnames(prompt_df)
-# 
-# 
-# # create corpus ------------------------------
-# 
-# 
-# # calling DataframeSource to save metadata,
-# # if you don't want to save metadata, just call VSource()
-# prompt_source <- DataframeSource(prompt_df)
-# prompt_corpus <- VCorpus(prompt_source)
-# 
-# prompt_corpus
-# 
-# # Looking at document number 10 (it can be any number)
-# prompt_corpus[[10]]
-# prompt_corpus[[10]][1] # the text of document 10
-# prompt_corpus[[10]][2] # the metadata of document 10
-# 
-# 
-# 
-# # clean the corpus ----------------------
-# 
-# # Remove punctuation
-# prompt_cleaned <- tm_map(prompt_corpus, removePunctuation)
-# 
-# # Remove numbers
-# prompt_cleaned <- tm_map(prompt_cleaned, removeNumbers)
-# 
-# # Make lowercase
-# prompt_cleaned <- tm_map(prompt_cleaned, content_transformer(tolower))
-# 
-# # Remove stop words
-# my_stops <- c(stopwords("en"),
-#               "just", "can", "like",
-#               "one", "youre", "get",
-#               "find", "day", "human",
-#               "humans", "world", "time",
-#               "life", "years", "now")
-# 
-# prompt_cleaned <- tm_map(prompt_cleaned, removeWords, my_stops)
-# 
-# # Remove white space
-# prompt_cleaned <- tm_map(prompt_cleaned, stripWhitespace)
-# 
-# 
-# # Checking the 3rd document's uncleaned text
-# prompt_corpus[[3]][1]
-# 
-# # Checking the 3rd document's cleaned text
-# prompt_cleaned[[3]][1]
-# 
-# 
-# 
-# 
-# 
-# 
-# #########################################################################
-# # TOPIC MODELING (DOCUMENT TERM MATRIX)
-# 
-# prompt_dtm <- DocumentTermMatrix(prompt_cleaned)
-# prompt_dtm
-# 
-# # Sparse entries are 0's, so a specific term does 
-# # not occur in that document.
-# # There are about 5,853 unique words across our documents.
-# # They don't appear in every document.
-# 
-# # a little more cleaning to remove empty rows
-# # keep only indexes of unique words
-# unique_indexes <- unique(prompt_dtm$i)
-# prompt_dtm <- prompt_dtm[unique_indexes,]
-# prompt_dtm
-# 
-# # turn our unique words into the
-# # tidy format of the tidyverse packages
-# prompt_dtm_tidy <- tidy(prompt_dtm)
-# prompt_dtm_tidy 
-# 
-# 
-# # LDA ANALYSIS ------------------------------------
-# 
-# # k is the number of topics we want
-# # (like k means clustering?)
-# 
-# k <- 3
-# 
-# # NOTE: Optimal k varies, so you'll need to experiment
-# # with which k value is best.
-# 
-# prompt_lda <- LDA(prompt_dtm, 
-#                  k = k, 
-#                  control = list(seed=42)) 
-# prompt_lda
-# 
-# # NOTE: seed keeps randomization consistent
-# 
-# # get the words out of our LDA
-# prompt_lda_words <- terms(prompt_lda, 5)
-# prompt_lda_words
-# 
-# # NOTE: at this stage you may want to go back 
-# # and add some stop words to your posts based
-# # on the results you got from LDA.
-# # Remove words that don't add any meaning
-# # to the analysis.
-# 
-# 
-# 
-# # Save LDA words as CSV --------------
-# 
-# # need a matrix format to be written to csv
-# prompt_lda_topics <- as.matrix(prompt_lda_words)
-# 
-# # write.csv(prompt_lda_topics,
-# #           file = paste("(new)prompt_LDA_",
-# #                        k,
-# #                        ".csv",
-# #                        sep = ""))
-# 
-# # NOTE: k variable is helpful here when you run many LDAs
-# # with different k's
-# 
-# 
-# 
-# # Examine the topics  --------------
-# 
-# head(prompt_lda_topics)
-# 
-# # turn our LDA into the
-# # tidy format of the tidyverse packages
-# prompt_lda_tidy <- tidy(prompt_lda)
-# 
-# # Beta shows how much each term 
-# # contributes to each topic
-# # NOTE: Typically list 8 terms when topic modelling
-# top_terms <- prompt_lda_tidy %>%
-#   group_by(topic) %>%
-#   top_n(8, beta) %>%
-#   ungroup() %>%
-#   arrange(topic, -beta)
-# head(top_terms, 10)
-# 
-# 
-# # Export to CSV
-# # 
-# # write.csv(top_terms,
-# #           file = paste("(new)01-prompt_LDA_",
-# #                        k,
-# #                        ".csv",
-# #                        sep = ""))
-# # 
-# 
-# 
-# # visualize with ggplot
-# # NOTE: We want to treat the topics as categorical,
-# # so turn it to a factor first.
-# # NOTE: Don't show legend because it's not helpful.
-# top_terms %>%
-#   mutate(term = reorder(term, beta)) %>%
-#   ggplot(aes(term, 
-#              beta, 
-#              fill = factor(topic))) +
-#   geom_col(show.legend=FALSE) +
-#   facet_wrap( ~ topic, scales="free") +
-#   coord_flip()
-# 
-# 
+
+##############################################################
+# CREATE CORPUS
+
+
+# create document id -------------------
+
+# note: for some reason this is quotes sensitive,
+# use double quotes
+names(df)[names(df)=="post_id"] <- "doc_id"
+df$id <- as.character(df$id)
+
+names(df)[names(df)=="post_and_comments"] <- "text"
+colnames(df)
+
+
+# create corpus ------------------------------
+
+
+# calling DataframeSource to save metadata,
+# if you don't want to save metadata, just call VSource()
+source <- DataframeSource(df)
+corpus <- VCorpus(source)
+
+corpus
+
+# Looking at document number 10 (it can be any number)
+corpus[[10]]
+corpus[[10]][1] # the text of document 10
+corpus[[10]][2] # the metadata of document 10
+
+
+# clean the corpus ----------------------
+
+# Remove punctuation
+cleaned <- tm_map(corpus, removePunctuation)
+
+# Remove numbers
+cleaned <- tm_map(cleaned, removeNumbers)
+
+# Make lowercase
+cleaned <- tm_map(cleaned, content_transformer(tolower))
+
+# Remove stop words
+my_stops <- c(stopwords("en"), 
+              "asian",
+              "asians",
+              "people",
+              "like",
+              "just",
+              "removed",
+              "dont",
+              "think",
+              "even",
+              "can",
+              "one",
+              "get",
+              "see",
+              "know",
+              "also",
+              "really",
+              "will")
+
+cleaned <- tm_map(cleaned, removeWords, my_stops)
+
+# Remove white space
+cleaned <- tm_map(cleaned, stripWhitespace)
+
+
+# Checking the 3rd document's uncleaned text
+corpus[[3]][1]
+
+# Checking the 3rd document's cleaned text
+cleaned[[3]][1]
+
+
+
+
+
+
+#########################################################################
+# TOPIC MODELING (DOCUMENT TERM MATRIX)
+
+dtm <- DocumentTermMatrix(cleaned)
+dtm
+
+# Sparse entries are 0's, so a specific term does
+# not occur in that document.
+# There are about 5,853 unique words across our documents.
+# They don't appear in every document.
+
+# a little more cleaning to remove empty rows
+# keep only indexes of unique words
+unique_indexes <- unique(dtm$i)
+dtm <- dtm[unique_indexes,]
+dtm
+
+# turn our unique words into the
+# tidy format of the tidyverse packages
+dtm_tidy <- tidy(dtm)
+dtm_tidy
+
+
+# LDA ANALYSIS ------------------------------------
+
+# k is the number of topics we want
+# (like k means clustering?)
+
+k <- 4
+
+# NOTE: Optimal k varies, so you'll need to experiment
+# with which k value is best.
+
+lda <- LDA(dtm,
+           k = k,
+           control = list(seed=42))
+lda
+
+# NOTE: seed keeps randomization consistent
+
+# get the words out of our LDA
+lda_words <- terms(lda, 5)
+lda_words
+
+# NOTE: at this stage you may want to go back
+# and add some stop words to your posts based
+# on the results you got from LDA.
+# Remove words that don't add any meaning
+# to the analysis.
+
+
+
+# Save LDA words as CSV --------------
+
+# need a matrix format to be written to csv
+lda_topics <- as.matrix(lda_words)
+
+# write.csv(lda_topics,
+#           file = paste("(new)LDA_",
+#                        k,
+#                        ".csv",
+#                        sep = ""))
+
+# NOTE: k variable is helpful here when you run many LDAs
+# with different k's
+
+
+
+# Examine the topics  --------------
+
+head(lda_topics)
+
+# turn our LDA into the
+# tidy format of the tidyverse packages
+lda_tidy <- tidy(lda)
+
+# Beta shows how much each term
+# contributes to each topic
+# NOTE: Typically list 8 terms when topic modelling
+top_terms <- lda_tidy %>%
+  group_by(topic) %>%
+  top_n(8, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta)
+head(top_terms, 10)
+
+
+# Export to CSV
+#
+# write.csv(top_terms,
+#           file = paste("(new)01-LDA_",
+#                        k,
+#                        ".csv",
+#                        sep = ""))
+#
+
+
+# visualize with ggplot
+# NOTE: We want to treat the topics as categorical,
+# so turn it to a factor first.
+# NOTE: Don't show legend because it's not helpful.
+top_terms %>%
+  mutate(term = reorder(term, beta)) %>%
+  ggplot(aes(term,
+             beta,
+             fill = factor(topic))) +
+  geom_col(show.legend=FALSE) +
+  facet_wrap( ~ topic, scales="free") +
+  coord_flip()
+
+
+##########################################################
+##########################################################
+##########################################################
+##########################################################
+##########################################################
+##########################################################
+##########################################################
+
 # 
 # # GAMMA -------------
 # 
 # # How much does each topic contribute to each document?
 # # --> Use GAMMA statistic
-# # (As opposed to how much each word contributes 
+# # (As opposed to how much each word contributes
 # # to each topic, which is beta)
 # 
-# prompt_lda_document_topics <- tidy(prompt_lda, 
+# lda_document_topics <- tidy(lda,
 #                                   matrix="gamma")
 # 
-# head(prompt_lda_document_topics)
-# tail(prompt_lda_document_topics)
+# head(lda_document_topics)
+# tail(lda_document_topics)
 # 
-# # write.csv(prompt_lda_document_topics,
-# #           file = paste("(new)prompt_topic_gamma_match",
+# # write.csv(lda_document_topics,
+# #           file = paste("(new)topic_gamma_match",
 # #                        k,
 # #                        ".csv",
 # #                        sep = ""))
 # 
 # # same as nrow() and ncol()
-# dim(prompt_lda_document_topics)
+# dim(lda_document_topics)
 # 
 # 
 # # Have each topic as a different column --------------
@@ -395,11 +333,11 @@ any(is.na(df))
 # # Ex: topic1, topic2, topic3
 # # As opposed to one topic column
 # # NOTE: gamma values are to fill in the new columns
-# prompt_lda_document <- spread(prompt_lda_document_topics, 
-#                              topic, # the col to spread 
+# lda_document <- spread(lda_document_topics,
+#                              topic, # the col to spread
 #                              gamma) # fill in the new cols with gamma
-# dim(prompt_lda_document)
-# head(prompt_lda_document)
+# dim(lda_document)
+# head(lda_document)
 # 
 # 
 # # Best Topic Match ----------------------------------
@@ -407,33 +345,33 @@ any(is.na(df))
 # # create column for the topic that has the
 # # greatest contribution to that document
 # 
-# prompt_lda_document$max_topic <- 
-#   colnames(prompt_lda_document[2:4])[apply(X=prompt_lda_document,
+# lda_document$max_topic <-
+#   colnames(lda_document[2:4])[apply(X=lda_document,
 #                                           MARGIN=1,
 #                                           FUN=which.max)]
 # 
 # 
 # # join tables together --------------------------------
 # 
-# dt1 <- data.table(prompt_lda_document, 
+# dt1 <- data.table(lda_document,
 #                   key = "document")
-# dt2 <- data.table(prompt_df, 
+# dt2 <- data.table(df,
 #                   key = "doc_id")
 # 
-# prompt_merged <- dt1[dt2]
-# dim(prompt_merged)
-# colnames(prompt_merged)
+# merged <- dt1[dt2]
+# dim(merged)
+# colnames(merged)
 # 
 # 
 # # select only the columns you want to work with
-# prompt_analyze <- select(prompt_merged,
+# analyze <- select(merged,
 #                          c(document,
 #                            text,
 #                            score,
 #                            num_comments,
 #                            url,
 #                            created))
-# head(prompt_analyze)
+# head(analyze)
 # 
 # 
 # 
@@ -455,15 +393,15 @@ any(is.na(df))
 # get_sentiments("bing")
 # 
 # 
-# # convert csv data into a format 
+# # convert csv data into a format
 # # the tidyverse library can work with
-# colnames(prompt_df)
+# colnames(df)
 # 
-# tidy_prompts <- prompt_df %>%
+# tidy_prompts <- df %>%
 #   ungroup() %>%
 #   unnest_tokens(word, text)
 # 
-# # from the conversion above, 
+# # from the conversion above,
 # # we have a new column called "word"
 # summary(tidy_prompts)
 # 
@@ -497,7 +435,7 @@ any(is.na(df))
 # 
 # 
 # # # Get all bing sentiments ---------------------------------------
-# # 
+# #
 # # write.csv(get_sentiments("bing"),
 # #           file = paste("bing_sentiments",
 # #                        k,
@@ -506,7 +444,7 @@ any(is.na(df))
 # 
 # 
 # # # Export CSV sentiment values ---------------------------------------
-# # 
+# #
 # # write.csv(prompts_sentiment,
 # #           file = paste("prompts_sentiments",
 # #                        k,
